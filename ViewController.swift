91ae17354f31f4ad9bef3ce526c512b2dbf85160
EstrelApps <carlmatheson779@gmail.com>
2016-12-08 08:11:05 +1100

    TestCommit
    This is a test commit

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MiraclePills
+//
+//  Created by Carl Matheson on 7/12/2016.
+//  Copyright © 2016 Carl Matheson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("test commit")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
